c9639a1b992ec9f393d649be254219d65d5060a2
HassanKhan123 <hk2602809@gmail.com>
2021-04-29 21:11:03 +0500

    web3 setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import React, { useEffect } from 'react';
+
+import web3 from './web3';
+
 function App() {
+  useEffect(() => {
+    (async () => {
+      const accounts = await web3.eth.getAccounts();
+      console.log('accounts====', accounts);
+    })();
+  });
+
   return <div>Solidity</div>;
 }
 
