a2329300cbeb75041eb5d7e8a34c362993aca01c
HassanKhan123 <hk2602809@gmail.com>
2021-04-29 21:44:22 +0500

    rendered contract data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import web3 from './web3';
+import lottery from './lottery';
 
 function App() {
+  const [manager, setManager] = useState('');
   useEffect(() => {
     (async () => {
       const accounts = await web3.eth.getAccounts();
       console.log('accounts====', accounts);
     })();
-  });
+  }, []);
 
-  return <div>Solidity</div>;
+  useEffect(() => {
+    (async () => {
+      const manager = await lottery.methods.manager().call();
+      setManager(manager);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This contract is managed by {manager} </p>
+    </div>
+  );
 }
 
 export default App;
