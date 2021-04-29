fdd123b726bc992395558f89d1408571a2553a8e
HassanKhan123 <hk2602809@gmail.com>
2021-04-29 22:04:40 +0500

    accessed balance and players properties

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import lottery from './lottery';
 
 function App() {
   const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
+
   useEffect(() => {
     (async () => {
       const accounts = await web3.eth.getAccounts();
@@ -15,14 +18,22 @@ function App() {
   useEffect(() => {
     (async () => {
       const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
       setManager(manager);
+      setPlayers(players);
+      setBalance(balance);
     })();
   }, []);
 
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager} </p>
+      <p>
+        This contract is managed by {manager}. There are currently{' '}
+        {players.length} people entered, competing to win{' '}
+        {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
     </div>
   );
 }
